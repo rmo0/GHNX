bab2ae7319e28eed134004976c44731f4a14b059
rmo0 <fh8pwn9k9x@privaterelay.appleid.com>
2026-02-01 00:25:03 +0900

    add cmd-line-args

--- ghnx.c
+++ ghnx.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include "ghnx.h"
 
-int main(void) {
+int main(int argc, char **argv) {
+    if (argc == 1) {
+        printf("引数が指定されていません。\n");
+    } else if (argc > 1) {
+        printf("引数が%dつ指定されています。\n", argc-=1);
+        for (int i = 0; i < argc; i++) {
+            printf("%s\n", argv[i + 1]);
+        }
+    }
     return 0;
 }
