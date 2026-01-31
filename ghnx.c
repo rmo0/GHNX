b8148bc066e91a14f7252de3da1bd3212f552e3c
rmo0 <fh8pwn9k9x@privaterelay.appleid.com>
2026-02-01 02:02:42 +0900

    add L5

--- ghnx.c
+++ ghnx.c
@@ -2,6 +2,6 @@
 #include "ghnx.h"
 
 int main(int argc, char **argv) {
-    if (argc == 1) return printf(""); 
+    if (argc == 1 || (argc == 2 && (argv[1] == "-h" || argv[1] == "--help"))) return printf(""); 
     return 0;
 }
