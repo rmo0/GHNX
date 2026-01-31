5ced5270f27197a1b428f2894a7fbb289847592c
rmo0 <fh8pwn9k9x@privaterelay.appleid.com>
2026-02-01 01:55:45 +0900

    add a message if argc == 1

--- ghnx.c
+++ ghnx.c
@@ -2,13 +2,6 @@
 #include "ghnx.h"
 
 int main(int argc, char **argv) {
-    if (argc == 1) {
-        printf("引数が指定されていません。\n");
-    } else if (argc > 1) {
-        printf("引数が%dつ指定されています。\n", argc-=1);
-        for (int i = 0; i < argc; i++) {
-            printf("%s\n", argv[i + 1]);
-        }
-    }
+    if (argc == 1) return printf(""); 
     return 0;
 }
