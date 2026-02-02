ba8817e74ff0cfa41982c8b27d37c93322d51f90
rmo0 <fh8pwn9k9x@privaterelay.appleid.com>
2026-02-02 09:26:55 +0900

    add prefix filter

--- source/main.c
+++ source/main.c
@@ -4,6 +4,8 @@
 #include "config.h"
 
 int main(int argc, char **argv) {
+    if (!(strcmp(argv[0], COMMAND_PREFIX) == 0 || strcmp(argv[0], FILENAME_PREFIX) == 0)) return 0;
+
     if (argc == 1 || (argc == 2 && (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0))) {
         return printf("usage: %s [-h | --help]\n", COMMAND_PREFIX);
     } else {
